66fa162cc7257f9a2479270aeb895fc38490a616
WeedleNoodle <predragstojkovic22@gmail.com>
2022-12-04 13:08:30 +0100

    Updated BoardContext
    {Boilerplate}

--- Sudoku_Autosolver/src/contexts/BoardContext.js
+++ Sudoku_Autosolver/src/contexts/BoardContext.js
@@ -12,56 +12,6 @@ export const boardReducer = (state, action) => {
       // console.log(state.board[action.payload.id - 1]);
       state.board[action.payload.id - 1].value = action.payload.value;
       return state;
-    case "SOLVE":
-      const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-      for (let i = 0; i < 81; i++) {
-        if (state.board[i].value !== "") {
-          state.board[i].inputed = true;
-        }
-      }
-
-      console.log(state.board);
-
-      //const z = state.board[0];
-      //for (let i = 0; i < 81; i++) {}
-
-      //main loop
-      for (let i = 0; i < 1; i++) {
-        //if (!inputed) {
-        let voidNumbers = [];
-        let x = state.board[i].x;
-        let y = state.board[i].y;
-        let group = state.board[i].group;
-        console.log("The tile being compared is: ", state.board[i]);
-
-        for (let j = 0; j < 81; j++) {
-          /*if (j === 0) {
-            console.log("state.board[0].value je", state.board[j].value);
-            console.log("state.board[0] je", state.board[j]);
-          }*/
-          if (
-            (y === state.board[j].y && state.board[j].value !== "") ||
-            (x === state.board[j].x && state.board[j].value !== "") ||
-            (group === state.board[j].group && state.board[j].value !== "")
-          ) {
-            console.log(state.board[j].value);
-            voidNumbers.push(Number(state.board[j].value));
-          }
-        }
-        console.log(voidNumbers);
-
-        for (let num of values) {
-          if (!voidNumbers.includes(num)) {
-            state.board[i].value = String(num);
-            break;
-          }
-        }
-        console.log(state.board);
-        console.log(state.board[i]);
-        console.log(state.board[i].value);
-      }
-      return state;
     default:
       return state;
   }
